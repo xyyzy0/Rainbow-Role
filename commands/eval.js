1f1a4043b8560dc5d430f34b5f573eb86bba5f88
Rawir <45668076+Rawiros@users.noreply.github.com>
2022-07-31 20:56:47 +0200

    Add files via upload

--- commands/eval.js
+++ commands/eval.js
@@ -1,16 +1,39 @@
 var { SlashCommandBuilder, codeBlock } = require('discord.js');
 var Utility = require('../libs/Utility');
 var Slash = require('../libs/Slash');
+const { default: fetch } = require('node-fetch');
 
 var Command = new Slash(
     {
         name: "eval",
         allowedUser: process.env.OWNER,
         builder: new SlashCommandBuilder().setDescription("🔧 Execute code").addStringOption(code => code.setName("code").setDescription("👨‍💻 Javascript Code").setRequired(true)),
-        executor: (Interaction, Client) => {
+        executor: async (Interaction, Client) => {
+            
+            /**
+             * @type {String}
+             */
             var code = Interaction.options.get("code").value??"null";
+            var PastebinPrefix = "pastebin:";
+            var URLPrefix = "url:";
+            var UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36";
 
             try {
+
+                if(code.startsWith(PastebinPrefix)) {
+                    var pastebinCode = code.slice(PastebinPrefix.length);
+                    var url = "https://pastebin.com/raw/" + pastebinCode;
+                    code = await (await fetch(url, { headers: { "user-agent": UA } })).text();
+                };
+
+                if(code.startsWith(URLPrefix)) {
+                    var url = code.slice(URLPrefix.length);
+
+                    code = await (await fetch(url, { headers: { "user-agent": UA } })).text();
+                };
+
+
+
                 var res = eval(code);
                 
                 if(typeof(res) === "object") {
