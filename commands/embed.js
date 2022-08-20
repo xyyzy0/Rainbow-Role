213e13fef0d8d01d31d40ae35275e1d6e50d45a3
xyyzy <73991019+xyyzy0@users.noreply.github.com>
2022-08-20 17:40:36 +0200

    🔑 v.0.0.8 update
    new command

---
+++ commands/embed.js
@@ -0,0 +1,29 @@
+var { SlashCommandBuilder, codeBlock } = require('discord.js');
+var Utility = require('../libs/Utility');
+var Slash = require('../libs/Slash');
+const { default: fetch } = require('node-fetch');
+
+var Command = new Slash(
+    {
+        name: "embed",
+        builder: new SlashCommandBuilder().setDescription("🗞 Send Embed").addStringOption(title => title.setName("title").setDescription("🏅 Embed title").setRequired(true)).addStringOption(description => description.setName("description").setDescription("🍃 Embed description").setRequired(true)),
+        executor: async (Interaction, Client) => {
+            
+            /**
+             * @type {String}
+             */
+            var title = Interaction.options.get("title").value??"null";
+            var description = Interaction.options.get("description").value??"null";
+            Interaction.reply({
+                embeds: [
+                    Utility.Embed(Interaction.member.user)
+                        .setTitle(new String(title))
+                        .setDescription(new String(description))
+                ]
+            })
+
+            }
+    }
+)
+
+module.exports = Command;
