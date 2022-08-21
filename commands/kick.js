045782d0b68a7267c0365574c1fd436ca027f744
xyyzy <73991019+xyyzy0@users.noreply.github.com>
2022-08-21 08:42:43 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,4 @@
-var { SlashCommandBuilder, codeBlock, PermissionFlagsBits} = require('discord.js');
+var { SlashCommandBuilder, codeBlock, PermissionFlagsBits } = require('discord.js');
 var Utility = require('../libs/Utility');
 var Slash = require('../libs/Slash');
 const { default: fetch } = require('node-fetch');
@@ -70,6 +70,5 @@ var Command = new Slash(
 
             }
     }
-)
 
 module.exports = Command;
