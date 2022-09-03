9e22772cd88fab0f400ff3f9011061ee80b39f57
xyyzy <73991019+xyyzy0@users.noreply.github.com>
2022-09-03 10:16:20 +0200

    🔑 v.1.0.0 update

--- commands/embed.js
+++ commands/embed.js
@@ -6,6 +6,7 @@ const { default: fetch } = require('node-fetch');
 var Command = new Slash(
     {
         name: "embed",
+        permissions: PermissionFlagsBits.ManageMessages,
         builder: new SlashCommandBuilder().setDescription("🗞 Send Embed").addStringOption(title => title.setName("title").setDescription("🏅 Embed title").setRequired(true)).addStringOption(description => description.setName("description").setDescription("🍃 Embed description").setRequired(true)),
         executor: async (Interaction, Client) => {
             var title = Interaction.options.get("title").value??"null";
